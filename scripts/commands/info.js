2f0f4e5ec100f20e145869649a4836309a793c8d
DAVIL KING <148178117+evaldavilhkr@users.noreply.github.com>
2024-08-03 11:55:57 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -39,7 +39,7 @@ var callback = () => api.sendMessage({body:`
 𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
 𝐆𝐦𝐚𝐢𝐥       : apon04860@gmail.com
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801717724407
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/MOHAMMADNAYAN
+𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/NAI
 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/Its.Baby.Apon`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(encodeURI(`https://graph.facebook.com/100082215656149/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
