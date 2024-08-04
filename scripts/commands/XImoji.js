a93f1ca4385cd35e99f01bb8b10b7579c69d9347
DAVIL KING <148178117+evaldavilhkr@users.noreply.github.com>
2024-08-04 09:59:18 +0600

    Update XImoji.js

--- scripts/commands/XImoji.js
+++ scripts/commands/XImoji.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 module.exports = {
   config:{
-	name: "🥴",
+	name: "😑",
         version: "1.0.1",
         prefix: false,
 	permssion: 0,
 	credits: "nayan", 
 	description: "Fun",
 	category: "no prefix",
-	usages: "🥴",
+	usages: "😑",
         cooldowns: 5, 
 },
 
@@ -16,12 +16,15 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
+	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😑")==0) {
+		var msg = {
+				
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("😁","⚠️","😡", event.messageID, (err) => {}, true)
+    api.setMessageReaction("😁","🐸","🧐","🥹","😑","🤦🏻‍♀️", event.messageID, (err) => {}, true)
 		}
 	},
 	start: function({ nayan }) {
 
   }
-                       }
+	    }
